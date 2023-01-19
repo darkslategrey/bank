07f4a0f7b563464fbe9decffa2c68ee0255076d0
Gregory Faruch <gregory.faruch@web-atrio.com>
2023-01-19 10:02:12 +0100

    fix(backend): env vars

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -2,46 +2,46 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("@nomiclabs/hardhat-etherscan");
 require("hardhat-deploy");
-require("hardhat-gas-reporter")
-require("solidity-coverage")
+require("hardhat-gas-reporter");
+require("solidity-coverage");
 
 const PK = process.env.PK || "";
-const ALCHEMY = process.env.ALCHEMY || "";
+const GOERLI_URL = process.env.GOERLI_URL || "";
 const ETHERSCAN = process.env.ETHERSCAN || "";
 
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     goerli: {
-      url: ALCHEMY,
+      url: GOERLI_URL,
       accounts: [`0x${PK}`],
       chainId: 5,
-      blockConfirmations: 6
+      blockConfirmations: 6,
     },
     localhost: {
       url: "http://127.0.0.1:8545",
-      chainId: 31337
-    }
+      chainId: 31337,
+    },
   },
-  solidity : {
+  solidity: {
     compilers: [
       {
-        version: "0.8.17"
-      }
-    ]
+        version: "0.8.17",
+      },
+    ],
   },
   etherscan: {
     apiKey: {
-      goerli: ETHERSCAN
-    }
+      goerli: ETHERSCAN,
+    },
   },
   namedAccounts: {
     deployer: {
       default: 0,
       1: 0,
-    }
+    },
   },
   gasReporter: {
-    enabled: true
-  }
+    enabled: true,
+  },
 };
